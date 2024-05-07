2c9a7c44268b7d7d74b72cef56907e27ee5d37a1
Marcin Fudala <nerko937@gmail.com>
2024-05-07 19:10:03 +0200

    lsp config improvements

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -4,6 +4,10 @@ return {
         lazy = false,
         config = function()
             require("lspconfig").pylsp.setup {
+                on_attach = function(client)
+                    vim.keymap.set("n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<CR>", {})
+                    vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", {})
+                end,
                 settings = {
                     pylsp = {
                         plugins = {
