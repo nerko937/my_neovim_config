10e15c9167207d15838277c56a35729d064ca07a
Marcin Fudala <nerko937@gmail.com>
2024-05-08 09:54:38 +0200

    Revert "moved minor_helpers to completions"
    This reverts commit 91c31fdf8b4da69688b191165f4e4be026f6b5a3.

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,14 +1,5 @@
 return {
     {
-    {"github/copilot.vim"},
-    {"jiangmiao/auto-pairs"},
-    {
-        "tpope/vim-commentary",
-        config = function()
-            vim.keymap.set("n", "<C-_>", ":Commentary<CR>", {})
-            vim.keymap.set("v", "<C-_>", ":Commentary<CR>", {})
-        end
-    },
         "hrsh7th/nvim-cmp",
         event = "InsertEnter",
         dependencies = {
