e3ae5d02f8406be91b87b58a55b6949d3fc45dac
Marcin Fudala <nerko937@gmail.com>
2024-05-18 07:44:50 +0200

    Changed history plugin to undotree

--- lua/plugins/fuzzy_finders.lua
+++ lua/plugins/fuzzy_finders.lua
@@ -23,16 +23,10 @@ return {
                 }
             }
             telescope.load_extension("fzf")
-            require("file_history").setup {
-                backup_dir = "~/.file-history-git",
-                git_cmd = "git"
-            }
-            telescope.load_extension("file_history")
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-            vim.keymap.set("n", "<leader>fh", ":Telescope file_history history<CR>", {}) --<C-r> on selected change to restore
-            vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+            vim.keymap.set("n", "<leader>fo", builtin.oldfiles, {})
             vim.keymap.set("n", "<leader>fr", builtin.lsp_references, {})
         end
     },
